099ec0f00e33780f7bfae7774b43a238aca96d1a
Evgen Kulbachenko <45nalitay45@gmail.com>
2025-04-08 13:59:11 +0300

    modal form complited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import AddCompanyButton from './components/add-company-button';
+import CompanyForm from './components/company-form';
+import StatusLabel, { Status } from './components/status-label';
+
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      {/* <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel> */}
+
+      <AddCompanyButton />
     </main>
   );
 }
