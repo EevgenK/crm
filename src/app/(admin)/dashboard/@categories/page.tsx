ff0d5cf7bc600c597d763f5987d96b9e458d52db
Evgen Kulbachenko <45nalitay45@gmail.com>
2025-04-24 17:49:17 +0300

    fixed for deploy

--- src/app/(admin)/dashboard/@categories/page.tsx
+++ src/app/(admin)/dashboard/@categories/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
   return (
     <DashboardCard label="Categories of companies">
       <div className="grid grid-cols-12 gap-3 pb-5 px-5">
-        {categories.map(({ id, title }) => (
+        {categories?.map(({ id, title }) => (
           <div key={id} className="col-span-3">
             <StatCard
               type={StatCardType.Dark}
